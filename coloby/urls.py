0b7118b1317b201d9816b8f872094a25e1a03c4e
Noah <noahtochukwu10@gmail.com>
2023-07-24 15:59:57 +0100

    Add Cowork Urls

--- coloby/urls.py
+++ coloby/urls.py
@@ -20,4 +20,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('cowork/', include('cowork.urls')),
 ]
