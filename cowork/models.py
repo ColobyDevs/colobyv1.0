3efefb73e5ffce3e533d0c64177922a0063644e4
Noah <noahtochukwu10@gmail.com>
2023-08-05 02:46:15 +0100

    Add file upload model

--- cowork/models.py
+++ cowork/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User
 
+
 class Room(models.Model):
     name = models.CharField(max_length=128)
     slug = models.SlugField(unique=True)
@@ -18,3 +19,36 @@ class Message(models.Model):
 
     def __str__(self):
         return (self.room.name + " - " + str(self.user.username) + " : " + str(self.message))
+
+
+class Task(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.RESTRICT)
+    # RESTRICT will delete the task if its parent is deleted
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.file.name
+
+class FileAccessLog(models.Model):
+    file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)
+    accessed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    accessed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.accessed_by.username} accessed {self.file.file.name} at {self.accessed_at} in {self.room.name}"
