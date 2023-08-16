547bc847cc0a81e001dc7c6dc5b2a59a1a7dabaa
Mayowa Ayeni <mayhoral008@gmail.com>
2023-08-16 17:39:49 +0100

    working filepath setups, app initialization and dependecies installation

---
+++ frontend/coloby-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './landingPage';
+
+function App() {
+  return (
+    <div className="App">
+    <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
