a77706c4e5a5e79f54049620a912fc812749a047
Noah <noahtochukwu10@gmail.com>
2023-09-01 14:33:31 +0100

    Create Register endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 import accounts.views as user_views
+from accounts import views
 
 urlpatterns = [
-    path("register/", user_views.register, name="register"),
+    path("register/", views.register_user, name="register"),
+    
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
