4affcdf273674141f19b8632b5aa61aec32a61eb
Noah <noahtochukwu10@gmail.com>
2023-09-01 14:37:36 +0100

    Update forms to use CustomUser model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,8 @@
-from accounts.models import User
+from accounts.models import CustomUser
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
 
     class Meta:
-        model= User
+        model= CustomUser
         fields = ['username', 'password1', 'password2']
