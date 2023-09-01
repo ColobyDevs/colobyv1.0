9caa166e37cbf6ec8205b68a140b6c8cfb26dc4f
Noah <noahtochukwu10@gmail.com>
2023-09-01 14:36:06 +0100

    Update register path to endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,15 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from accounts.forms import UserRegisterForm
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
 
-
-def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            messages.success(
-                request, f"Your account has been created and you are now logged in as {username}"
-            )
-            return redirect("index")
-    else:
-        form = UserRegisterForm()
-    return render(request, "accounts/register.html", {"form": form})
-
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- coloby/urls.py
+++ coloby/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include('accounts.urls')),
+    path("api/auth/", include('accounts.urls')),
     path("", include('cowork.urls')),
     path('views', TemplateView.as_view(template_name='index.html')),
 ]
