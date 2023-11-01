c7f8137f5a2cf179aa7d9fb15b1c0874a55b2de2
Noah <76827471+Noah-droid@users.noreply.github.com>
2023-11-01 11:11:19 +0100

    Update settings.py

--- coloby/settings.py
+++ coloby/settings.py
@@ -118,13 +118,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    #  os.path.join(BASE_DIR, "frontend/build/static"),
-]
-
+STATIC_URL = '/static/'
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
 MEDIA_URL = "media/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
