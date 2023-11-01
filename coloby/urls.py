6332a564b890c7bdd7fc7d21cb0c23e997d8eea9
Noah <76827471+Noah-droid@users.noreply.github.com>
2023-11-01 11:09:28 +0100

    Update urls.py

--- coloby/urls.py
+++ coloby/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
         
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 # if settings.DEBUG:
