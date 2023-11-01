f26332aa363d8efba0b1513f88dd44af88ab9e04
Noah <76827471+Noah-droid@users.noreply.github.com>
2023-11-01 02:24:49 +0100

    Update settings.py

--- coloby/settings.py
+++ coloby/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     "daphne",
